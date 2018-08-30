a3efc7840f515b024bc51b432fb11404f4cb444f
Bo Leng <bleng@ualberta.ca>
2018-08-30 16:53:59 -0600

    arxiv mail feeder

---
+++ arxivFeed/private_variables.py
@@ -0,0 +1,13 @@
+my_mail = "blengnb@gmail.com"
+
+# Search for these words only in the title
+title_words = []
+
+# Search for these words in the title and abstract
+abstract_words = ["field", "positronium", "tetraquark"]
+
+# Return all articles by these authors
+author_words = []
+
+# The url of the feed that is watched
+feed_url = 'http://arxiv.org/rss/hep-ph'
